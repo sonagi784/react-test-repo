f0dc528e8346964060e86523fd41c80b64bbdaab
sonagi784 <sonagi784@gmail.com>
2023-03-07 01:47:12 +0900

    refactor

--- src/components/home/SearchForm.tsx
+++ src/components/home/SearchForm.tsx
@@ -1,46 +1,54 @@
 import { useState } from 'react';
-import { Form, Button, InputGroup, Container } from 'react-bootstrap';
+import { Form, Button, InputGroup } from 'react-bootstrap';
 
 export default function SearchForm({ label = '', placeholder = '', button = '검색' }) {
   const [searchValue, setSearchValue] = useState('');
   const [isFormError, setFormError] = useState(false);
+
+  const goSearch = () => {
+    if (searchValue.length <= 0) {
+      setFormError(true);
+      return;
+    }
+
+    console.log('search: ', searchValue);
+  };
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    goSearch();
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      goSearch();
+    }
+  };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchValue(e.target.value);
     setFormError(false);
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    if (searchValue.length <= 0) {
-      setFormError(true);
-      return;
-    }
-
-    setFormError(false);
-    console.log(searchValue);
-  };
-
   return (
-    <Container>
-      <Form className="m-5" onSubmit={handleSubmit}>
-        <Form.Group className="text-center">
-          {label && <Form.Label className="d-block">{label}</Form.Label>}
-          <InputGroup size="lg">
-            <Form.Control
-              type="search"
-              placeholder={placeholder}
-              value={searchValue}
-              isInvalid={isFormError}
-              onChange={handleChange}
-            />
-            <Button variant="primary" type="submit">
-              {button}
-            </Button>
-          </InputGroup>
-        </Form.Group>
-      </Form>
-    </Container>
+    <Form className="m-5" onSubmit={handleSubmit}>
+      <Form.Group className="text-center">
+        {label && <Form.Label className="d-block">{label}</Form.Label>}
+        <InputGroup size="lg">
+          <Form.Control
+            type="search"
+            placeholder={placeholder}
+            value={searchValue}
+            isInvalid={isFormError}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+          />
+          <Button variant="primary" type="submit">
+            {button}
+          </Button>
+        </InputGroup>
+      </Form.Group>
+    </Form>
   );
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,10 @@
+import { Container } from 'react-bootstrap';
 import SearchForm from '@/components/home/SearchForm';
 
 export default function Home() {
-  return <SearchForm label="키워드를 입력하면, 이미지를 찾아드려요" placeholder="키워드" />;
+  return (
+    <Container>
+      <SearchForm label="키워드를 입력하면, 이미지를 찾아드려요" placeholder="키워드" />
+    </Container>
+  );
 }
